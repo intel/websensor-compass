6f40c3fe0049b50f3bd8e3e79715e5dcd255fe2a
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2016-08-31 14:21:38 +0200

    initial port to Generic Sensor API

--- scripts/compass.js
+++ scripts/compass.js
@@ -25,6 +25,405 @@
   * URLs:      http://www.pierrox.net/cmsms/applications/marine-compass.html
   */
 
+var M_PI   = Math.PI;
+var M_PI_2 = M_PI / 2;
+var M_2_PI = 2 * M_PI;
+
+var degToRad = M_PI / 180;
+var radToDeg = 180 / M_PI;
+
+class RotationMatrix {
+  constructor(m11, m12, m13, m21, m22, m23, m31, m32, m33) {
+	var outMatrix;
+
+	this.elements = new Float32Array(9);
+	this.set(m11, m12, m13, m21, m22, m23, m31, m32, m33);
+  }
+
+  set(m11, m12, m13, m21, m22, m23, m31, m32, m33) {
+	this.elements[ 0 ] = m11 || 1;
+	this.elements[ 1 ] = m12 || 0;
+	this.elements[ 2 ] = m13 || 0;
+	this.elements[ 3 ] = m21 || 0;
+	this.elements[ 4 ] = m22 || 1;
+	this.elements[ 5 ] = m23 || 0;
+	this.elements[ 6 ] = m31 || 0;
+	this.elements[ 7 ] = m32 || 0;
+	this.elements[ 8 ] = m33 || 1;
+  }
+
+  copy(matrix) {
+	this.elements[ 0 ] = matrix.elements[ 0 ];
+	this.elements[ 1 ] = matrix.elements[ 1 ];
+	this.elements[ 2 ] = matrix.elements[ 2 ];
+	this.elements[ 3 ] = matrix.elements[ 3 ];
+	this.elements[ 4 ] = matrix.elements[ 4 ];
+	this.elements[ 5 ] = matrix.elements[ 5 ];
+	this.elements[ 6 ] = matrix.elements[ 6 ];
+	this.elements[ 7 ] = matrix.elements[ 7 ];
+	this.elements[ 8 ] = matrix.elements[ 8 ];
+  }
+
+  identity() {
+    this.set(1, 0, 0,
+			 0, 1, 0,
+			 0, 0, 1);
+
+	return this;
+  }
+
+  setFromEuler(euler) {
+	let _x, _y, _z;
+	let cX, cY, cZ, sX, sY, sZ;
+
+	euler = euler || {};
+
+	_z = ( euler.alpha || 0 ) * degToRad;
+	_x = ( euler.beta || 0 ) * degToRad;
+	_y = ( euler.gamma || 0 ) * degToRad;
+
+	cX = Math.cos( _x );
+	cY = Math.cos( _y );
+	cZ = Math.cos( _z );
+	sX = Math.sin( _x );
+	sY = Math.sin( _y );
+	sZ = Math.sin( _z );
+
+	//
+	// ZXY-ordered rotation matrix construction.
+	//
+
+	this.set(
+	  cZ * cY - sZ * sX * sY, // 1,1
+	  - cX * sZ,              // 1,2
+	  cY * sZ * sX + cZ * sY, // 1,3
+
+	  cY * sZ + cZ * sX * sY, // 2,1
+	  cZ * cX,                // 2,2
+	  sZ * sY - cZ * cY * sX, // 2,3
+
+	  - cX * sY,              // 3,1
+	  sX,                     // 3,2
+	  cX * cY                 // 3,3
+	);
+
+	this.normalize();
+	return this;
+  }
+
+  setFromQuaternion(q) {
+    let sqw, sqx, sqy, sqz;
+
+    sqw = q.w * q.w;
+	sqx = q.x * q.x;
+	sqy = q.y * q.y;
+	sqz = q.z * q.z;
+
+	this.set(
+  	  sqw + sqx - sqy - sqz,       // 1,1
+	  2 * (q.x * q.y - q.w * q.z), // 1,2
+	  2 * (q.x * q.z + q.w * q.y), // 1,3
+
+	  2 * (q.x * q.y + q.w * q.z), // 2,1
+	  sqw - sqx + sqy - sqz,       // 2,2
+	  2 * (q.y * q.z - q.w * q.x), // 2,3
+
+	  2 * (q.x * q.z - q.w * q.y), // 3,1
+	  2 * (q.y * q.z + q.w * q.x), // 3,2
+	  sqw - sqx - sqy + sqz        // 3,3
+	);
+
+	return this;
+  }
+
+  static multiplyMatrices(a, b) {
+	let matrix = new RotationMatrix();
+
+	let aE, bE;
+
+	aE = a.elements;
+	bE = b.elements;
+
+	matrix.set(
+	  aE[0] * bE[0] + aE[1] * bE[3] + aE[2] * bE[6],
+	  aE[0] * bE[1] + aE[1] * bE[4] + aE[2] * bE[7],
+	  aE[0] * bE[2] + aE[1] * bE[5] + aE[2] * bE[8],
+
+	  aE[3] * bE[0] + aE[4] * bE[3] + aE[5] * bE[6],
+  	  aE[3] * bE[1] + aE[4] * bE[4] + aE[5] * bE[7],
+	  aE[3] * bE[2] + aE[4] * bE[5] + aE[5] * bE[8],
+
+	  aE[6] * bE[0] + aE[7] * bE[3] + aE[8] * bE[6],
+	  aE[6] * bE[1] + aE[7] * bE[4] + aE[8] * bE[7],
+	  aE[6] * bE[2] + aE[7] * bE[5] + aE[8] * bE[8]
+	);
+
+	return matrix;
+  }
+
+  static normalize(matrix) {
+	let R = matrix.elements;
+
+	// Calculate matrix determinant
+	let determinant = R[0] * R[4] * R[8] - R[0] * R[5] * R[7] - R[1] * R[3] * R[8] + R[1] * R[5] * R[6] + R[2] * R[3] * R[7] - R[2] * R[4] * R[6];
+
+	// Normalize matrix values
+	R[0] /= determinant;
+	R[1] /= determinant;
+	R[2] /= determinant;
+	R[3] /= determinant;
+	R[4] /= determinant;
+	R[5] /= determinant;
+	R[6] /= determinant;
+	R[7] /= determinant;
+	R[8] /= determinant;
+
+	matrix.elements = R;
+
+	return matrix;
+  }
+
+  static rotateByAxisAngle(targetRotationMatrix, axis, angle) {
+	let outputMatrix = new RotationMatrix();
+	let transformMatrix = new RotationMatrix();
+
+	let sA, cA;
+	let validAxis = false;
+
+	transformMatrix.identity(); // reset transform matrix
+
+	validAxis = false;
+
+	sA = Math.sin( angle );
+	cA = Math.cos( angle );
+
+	if ( axis[ 0 ] === 1 && axis[ 1 ] === 0 && axis[ 2 ] === 0 ) { // x
+	  validAxis = true;
+
+	  transformMatrix.elements[4] = cA;
+	  transformMatrix.elements[5] = -sA;
+	  transformMatrix.elements[7] = sA;
+	  transformMatrix.elements[8] = cA;
+	} else if ( axis[ 1 ] === 1 && axis[ 0 ] === 0 && axis[ 2 ] === 0 ) { // y
+      validAxis = true;
+
+	  transformMatrix.elements[0] = cA;
+	  transformMatrix.elements[2] = sA;
+	  transformMatrix.elements[6] = -sA;
+	  transformMatrix.elements[8] = cA;
+	} else if ( axis[ 2 ] === 1 && axis[ 0 ] === 0 && axis[ 1 ] === 0 ) { // z
+	  validAxis = true;
+
+	  transformMatrix.elements[0] = cA;
+	  transformMatrix.elements[1] = -sA;
+	  transformMatrix.elements[3] = sA;
+	  transformMatrix.elements[4] = cA;
+    }
+
+	if ( validAxis ) {
+	  outputMatrix = this.multiplyMatrices( targetRotationMatrix, transformMatrix );
+      outputMatrix = this.normalize( outputMatrix );
+    } else {
+      outputMatrix = targetRotationMatrix;
+    }
+
+    return outputMatrix;
+  }
+
+  multiply(m) {
+	let outMatrix = this.multiplyMatrices( this, m );
+	this.copy( outMatrix );
+
+	return this;
+  }
+
+  rotateX(angle) {
+	let outMatrix = RotationMatrix.rotateByAxisAngle( this, [ 1, 0, 0 ], angle );
+	this.copy( outMatrix );
+
+	return this;
+  };
+
+  rotateY(angle) {
+    let outMatrix = RotationMatrix.rotateByAxisAngle( this, [ 0, 1, 0 ], angle );
+	this.copy( outMatrix );
+
+	return this;
+  }
+
+  rotateZ(angle) {
+	let outMatrix = RotationMatrix.rotateByAxisAngle( this, [ 0, 0, 1 ], angle );
+	this.copy( outMatrix );
+
+	return this;
+  }
+
+  normalize() {
+	return RotationMatrix.normalize( this );
+  }
+};
+
+
+
+class Euler{
+  constructor(alpha, beta, gamma) {
+	this.set( alpha, beta, gamma );
+  }
+
+  set(alpha, beta, gamma) {
+	this.alpha = alpha || 0;
+	this.beta  = beta  || 0;
+	this.gamma = gamma || 0;
+  }
+
+  copy(inEuler) {
+	this.alpha = inEuler.alpha;
+	this.beta  = inEuler.beta;
+	this.gamma = inEuler.gamma;
+  }
+
+  setFromRotationMatrix(matrix) {
+    let R, _alpha, _beta, _gamma;
+
+	R = matrix.elements;
+
+	if (R[8] > 0) { // cos(beta) > 0
+
+		_alpha = Math.atan2(-R[1], R[4]);
+		_beta  = Math.asin(R[7]); // beta (-pi/2, pi/2)
+		_gamma = Math.atan2(-R[6], R[8]); // gamma (-pi/2, pi/2)
+
+	} else if (R[8] < 0) {  // cos(beta) < 0
+
+		_alpha = Math.atan2(R[1], -R[4]);
+		_beta  = -Math.asin(R[7]);
+		_beta  += (_beta >= 0) ? - M_PI : M_PI; // beta [-pi,-pi/2) U (pi/2,pi)
+		_gamma = Math.atan2(R[6], -R[8]); // gamma (-pi/2, pi/2)
+
+	} else { // R[8] == 0
+	  if (R[6] > 0) {  // cos(gamma) == 0, cos(beta) > 0
+			_alpha = Math.atan2(-R[1], R[4]);
+			_beta  = Math.asin(R[7]); // beta [-pi/2, pi/2]
+			_gamma = - M_PI_2; // gamma = -pi/2
+	  } else if (R[6] < 0) { // cos(gamma) == 0, cos(beta) < 0
+			_alpha = Math.atan2(R[1], -R[4]);
+			_beta  = -Math.asin(R[7]);
+			_beta  += (_beta >= 0) ? - M_PI : M_PI; // beta [-pi,-pi/2) U (pi/2,pi)
+			_gamma = - M_PI_2; // gamma = -pi/2
+	  } else { // R[6] == 0, cos(beta) == 0
+			// gimbal lock discontinuity
+			_alpha = Math.atan2(R[3], R[0]);
+			_beta  = (R[7] > 0) ? M_PI_2 : - M_PI_2; // beta = +-pi/2
+			_gamma = 0; // gamma = 0
+	  }
+	}
+
+	// alpha is in [-pi, pi], make sure it is in [0, 2*pi).
+	if (_alpha < 0) {
+	  _alpha += M_2_PI; // alpha [0, 2*pi)
+	}
+
+	// Convert to degrees
+	_alpha *= radToDeg;
+	_beta  *= radToDeg;
+	_gamma *= radToDeg;
+
+	// apply derived euler angles to current object
+	this.set(_alpha, _beta, _gamma);
+  }
+
+  setFromQuaternion(q) {
+    let _alpha, _beta, _gamma;
+
+	var sqw = q.w * q.w;
+	var sqx = q.x * q.x;
+	var sqy = q.y * q.y;
+	var sqz = q.z * q.z;
+
+	var unitLength = sqw + sqx + sqy + sqz; // Normalised == 1, otherwise correction divisor.
+	var wxyz = q.w * q.x + q.y * q.z;
+	var epsilon = 1e-6; // rounding factor
+
+			if (wxyz > (0.5 - epsilon) * unitLength) {
+
+				_alpha = 2 * Math.atan2(q.y, q.w);
+				_beta = M_PI_2;
+				_gamma = 0;
+
+			} else if (wxyz < (-0.5 + epsilon) * unitLength) {
+
+				_alpha = -2 * Math.atan2(q.y, q.w);
+				_beta = -M_PI_2;
+				_gamma = 0;
+
+			} else {
+
+				var aX = sqw - sqx + sqy - sqz;
+				var aY = 2 * (q.w * q.z - q.x * q.y);
+
+				var gX = sqw - sqx - sqy + sqz;
+				var gY = 2 * (q.w * q.y - q.x * q.z);
+
+				if (gX > 0) {
+
+					_alpha = Math.atan2(aY, aX);
+					_beta  = Math.asin(2 * wxyz / unitLength);
+					_gamma = Math.atan2(gY, gX);
+
+				} else {
+
+					_alpha = Math.atan2(-aY, -aX);
+					_beta  = -Math.asin(2 * wxyz / unitLength);
+					_beta  += _beta < 0 ? M_PI : - M_PI;
+					_gamma = Math.atan2(-gY, -gX);
+
+				}
+
+			}
+
+			// alpha is in [-pi, pi], make sure it is in [0, 2*pi).
+			if (_alpha < 0) {
+				_alpha += M_2_PI; // alpha [0, 2*pi)
+			}
+
+			// Convert to degrees
+			_alpha *= radToDeg;
+			_beta  *= radToDeg;
+			_gamma *= radToDeg;
+
+			// apply derived euler angles to current object
+	this.set( _alpha, _beta, _gamma );
+  }
+
+  rotateX(angle) {
+	Euler.rotateByAxisAngle( this, [ 1, 0, 0 ], angle );
+	return this;
+  }
+
+  rotateY(angle) {
+    Euler.rotateByAxisAngle( this, [ 0, 1, 0 ], angle );
+	return this;
+  }
+
+  rotateZ(angle) {
+	Euler.rotateByAxisAngle( this, [ 0, 0, 1 ], angle );
+	return this;
+  }
+
+  static rotateByAxisAngle(targetEuler, axis, angle) {
+    let _matrix = new RotationMatrix();
+	let outEuler;
+
+	_matrix.setFromEuler( targetEuler );
+	_matrix = RotationMatrix.rotateByAxisAngle( _matrix, axis, angle );
+
+	targetEuler.setFromRotationMatrix( _matrix );
+
+	return targetEuler;
+  }
+};
+
+
 !(function(window, undefined) {
 
     var document = window.document;
@@ -100,27 +499,20 @@
 
         try {
             this.gl = create3DContext(canvasElement);
-
             if(!this.gl) {
-
               this.output('Unable to initialize WebGL. Your browser may not support it', 'http://get.webgl.org');
-
             } else {
-
               this.gl.viewportWidth = canvasElement.getAttribute('width');
               this.gl.viewportHeight = canvasElement.getAttribute('height');
 
               this.init();
 
               this.render();
-
             }
-
         }
         catch(e) {
-
+          console.log(e);
           this.output('Unable to initialize WebGL. Your browser may not support it', 'http://get.webgl.org');
-
         }
 
     };
@@ -133,9 +525,36 @@
 
             var self = this;
 
-            // Set up *compass* deviceorientation data capture via Full Tilt JS
-            self.orientationData = new FULLTILT.DeviceOrientation({'type': 'world'});
-            self.orientationData.start();
+            self.accel = new AccelerometerSensor({ frequency: 60, includesGravity: true });
+            self.gyros = new GyroscopeSensor({ frequency: 60 });
+            self.accel.start();
+            self.gyros.start();
+
+            let xGyro = this.gyros.reading.rotationRateX;      
+            let yGyro = this.gyros.reading.rotationRateY;
+            let zGyro = this.gyros.reading.rotationRateZ;
+
+            let timestamp = this.gyros.reading.timeStamp;
+
+            self.gyros.onchange = event => {
+              // http://www.instructables.com/id/Accelerometer-Gyro-Tutorial/step3/Combining-the-Accelerometer-and-Gyro/
+              let gyroTrust = 0.98;
+
+              let xAccel = this.accel.reading.accelerationX;
+              let yAccel = this.accel.reading.accelerationY;
+              let zAccel = this.accel.reading.accelerationZ;
+
+              let dt = (this.gyros.reading.timeStamp - timestamp) / 10000;
+              timestamp = this.gyros.reading.timeStamp;
+
+              xGyro = xGyro + this.gyros.reading.rotationRateX * dt;
+              yGyro = yGyro + this.gyros.reading.rotationRateY * dt;
+              zGyro = zGyro + this.gyros.reading.rotationRateZ * dt;
+
+              self.alpha = zGyro * gyroTrust + zAccel * (1 - gyroTrust);
+              self.beta = xGyro * gyroTrust + xAccel * (1 - gyroTrust);
+              self.gamma = yGyro * gyroTrust + yAccel * (1 - gyroTrust);
+            }
 
             // Create rotation matrix object (calculated per canvas draw)
             this.rotationMatrix = mat4.create();
@@ -224,14 +643,18 @@
             }
         },
 
-    	calculateRotationMatrix: function() {
+    	calculateRotationMatrix: function() {           
+            let euler = new Euler();
+            euler.set(
+			  this.alpha,
+			  this.beta,
+			  this.gamma
+			);
 
-            if(!this.orientationData) return;
+            let orientationMatrix = new RotationMatrix();
+			orientationMatrix.setFromEuler(euler);
 
-            // Pull the latest deviceorientation rotation matrix from Full Tilt JS
-            var orientationMatrix = this.orientationData.getScreenAdjustedMatrix();
-
-            // Copy 3x3 FULLTILT.RotationMatrix values to 4x4 gl-matrix mat4
+            // Copy 3x3 RotationMatrix values to 4x4 gl-matrix mat4
             this.rotationMatrix[0] = orientationMatrix.elements[0];
             this.rotationMatrix[1] = orientationMatrix.elements[1];
             this.rotationMatrix[2] = orientationMatrix.elements[2];
@@ -250,21 +673,12 @@
 
             this.mCompassRenderer.setRotationMatrix(this.rotationMatrix);
 
-            // only show the heading if alpha !== null in used raw data
-            var rawOrientationData = this.orientationData.getLastRawEventData();
-            if(rawOrientationData.alpha !== undefined && rawOrientationData.alpha !== null) {
+            euler.setFromRotationMatrix(orientationMatrix);
+            this.mCompassRenderer.setCompassHeading(360 - euler.alpha);
 
-                // calculate compass heading pointing out of the back of the screen
-                var euler = new FULLTILT.Euler();
-                euler.setFromRotationMatrix(orientationMatrix);
-
-                this.mCompassRenderer.setCompassHeading( 360 - euler.alpha );
-
-            }
     	},
 
         render: function() {
-
             // Update orientation buffer
             this.calculateRotationMatrix();
 
