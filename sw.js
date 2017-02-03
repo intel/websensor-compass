8c8b328d788dd8438e64b15a30551131a0e528de
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2017-02-03 13:25:10 +0100

    Remove all caches on update

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
 self.addEventListener('install', function(event) {
 });
 
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   if (event.request.url.includes('manifest.json')) {
     event.respondWith(fetch(event.request));
